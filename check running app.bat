2107a133d8fb6c2e6c830d30a406647ef340507b
anrsaad <anrsaad@gmail.com>
2021-08-17 02:50:12 +0200

    Create check running app.bat

---
+++ check running app.bat
@@ -0,0 +1,7 @@
+@echo off
+title chrome blocker
+:main
+tasklist /fi "ImageName eq chrome.exe" /fo csv 2>NUL | find /I "chrome.exe">NUL
+if "%ERRORLEVEL%"=="0" TASKKILL /F /IM chrome.exe
+goto main 
+                          
